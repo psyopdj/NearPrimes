e18ce79209a4f0f1f2fe4dceb08db570a03df7ce
psyopdj <psyopdj@gmail.com>
2017-08-13 18:32:31 -0500

    Added a few comments to explain certain decisions.

--- nearprimes/src/nearprimes/Primes.java
+++ nearprimes/src/nearprimes/Primes.java
@@ -8,10 +8,13 @@ public class Primes {
 		
 		long n;
 		Scanner reader = new Scanner(System.in);
+		
+		/*	This loop will read in long integers and check if they are prime until
+			the user enters '0'. */
 		do {
 			System.out.print("Enter a number: ");
 			n = reader.nextLong();
-			long startTime = System.currentTimeMillis();
+			long startTime = System.currentTimeMillis();		// This variable will keep track of starting time for us
 			if (n < 0) {
 				System.out.println("You entered an invalid number.");
 			}
@@ -21,7 +24,7 @@ public class Primes {
 			else{
 				if ((n % 2) != 0){
 					boolean isPrime = true;
-					for (long i = 3; i <= (n/2); i += 2){
+					for (long i = 3; i <= (n/2); i += 2){		// Only need to check if it's divisible by odd numbers
 						if ((n % i) == 0){
 							isPrime = false;
 							break;
@@ -30,12 +33,12 @@ public class Primes {
 							isPrime = true;
 						}
 					}
-					if (isPrime == true){
+					if (isPrime == true){		// The most efficient possibility is for the entered number to already be prime so we check for that first
 						System.out.println(n + " is prime.");
 						long endTime1 = System.currentTimeMillis();
 						System.out.println("This was done in " + ((endTime1 - startTime)) + " milliseconds.");
 					}
-					else if (isPrime == false){
+					else if (isPrime == false){			// If the number wasn't prime, get low-prime and high-prime.
 						long lowPrime = getLowPrime(n);
 						long highPrime = getHighPrime(n);
 						System.out.println(lowPrime + " < " + n + " < " + highPrime);
@@ -44,12 +47,12 @@ public class Primes {
 					}
 				}
 				else{
-					if (n == 2){
+					if (n == 2){		// Unique case that can be checked easily and efficiently.
 						System.out.println(n + " is prime.");
 						long endTime3 = System.currentTimeMillis();
 						System.out.println("This was done in " + ((endTime3 - startTime)) + " milliseconds.");
 					}
-					else{
+					else{		// If n was even but not '2', get low-prime and high-prime.
 						long lowPrime = getLowPrime(n);
 						long highPrime = getHighPrime(n);
 						System.out.println(lowPrime + " < " + n + " < " + highPrime);
@@ -64,6 +67,10 @@ public class Primes {
 
 	}
 	
+	/*	This function takes in a long and returns the nearest prime less than the long.
+		Since primes are only odd (except for 2), the function only needs to check if
+		its divisible by odd numbers (because every odd number is the product of only
+		odd numbers). */
 	public static long getLowPrime(long num){
 		long newPrime;
 		boolean isFound = false;
@@ -92,6 +99,10 @@ public class Primes {
 		return newPrime;
 	}
 	
+	/*	This function takes in a long and returns the nearest prime more than the long.
+	Since primes are only odd (except for 2), the function only needs to check if
+	its divisible by odd numbers (because every odd number is the product of only
+	odd numbers). */
 	public static long getHighPrime(long num){
 		long newPrime;
 		boolean isFound = false;
